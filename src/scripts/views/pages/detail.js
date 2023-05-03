1b750feeb503ddedcc433ac3bd56dac983bca471
AdrianW <adrianwijaya584@gmail.com>
2023-05-03 09:03:20 +0800

    update offline detail page

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -20,44 +20,49 @@ export default {
     const mainWrapper= document.querySelector('#mainWrapper');
     mainWrapper.innerHTML= createDetailPageSkeleton();
 
-    const {restaurant}= await restaurantsource.getRestaurantDetail(id);
+    try {
+      const {restaurant}= await restaurantsource.getRestaurantDetail(id);
 
-    mainWrapper.innerHTML= createDetailPageCard(restaurant);
+      mainWrapper.innerHTML= createDetailPageCard(restaurant);
 
-    favoriteButtonPresenter.init({
-      favoriteButtonSection: document.querySelector('#favoriteButtonContainer'),
-      favoriteRestaurantsDb: idb,
-      restaurant: {
-        id: restaurant.id,
-        pictureId: restaurant.pictureId,
-        name: restaurant.name,
-        address: restaurant.address,
-        city: restaurant.city,
-        rating: restaurant.rating,
-        description: restaurant.description,
-        menus: restaurant.menus,
-        customerReviews: restaurant.customerReviews,
-      },
-    });
-
-    const reviewCards= document.querySelector('#reviewCards');
-    const form= document.querySelector('form');
-    const name= document.querySelector('#nameInput');
-    const review= document.querySelector('#reviewInput');
-
-    form.addEventListener('submit', async (event)=> {
-      event.preventDefault();
-      const reviewsData= await restaurantsource.postReview({
-        id,
-        name: name.value,
-        review: review.value,
+      favoriteButtonPresenter.init({
+        favoriteButtonSection:
+          document.querySelector('#favoriteButtonContainer'),
+        favoriteRestaurantsDb: idb,
+        restaurant: {
+          id: restaurant.id,
+          pictureId: restaurant.pictureId,
+          name: restaurant.name,
+          address: restaurant.address,
+          city: restaurant.city,
+          rating: restaurant.rating,
+          description: restaurant.description,
+          menus: restaurant.menus,
+          customerReviews: restaurant.customerReviews,
+        },
       });
 
-      name.value= '';
-      review.value= '';
+      const reviewCards= document.querySelector('#reviewCards');
+      const form= document.querySelector('form');
+      const name= document.querySelector('#nameInput');
+      const review= document.querySelector('#reviewInput');
 
-      reviewCards.innerHTML=
-        createRestaurantReviewsCard(reviewsData.customerReviews);
-    });
+      form.addEventListener('submit', async (event)=> {
+        event.preventDefault();
+        const reviewsData= await restaurantsource.postReview({
+          id,
+          name: name.value,
+          review: review.value,
+        });
+
+        name.value= '';
+        review.value= '';
+
+        reviewCards.innerHTML=
+          createRestaurantReviewsCard(reviewsData.customerReviews);
+      });
+    } catch (error) {
+      mainWrapper.innerHTML= `<p>Terjadi kesalahan di server.</p>`;
+    }
   },
 };
